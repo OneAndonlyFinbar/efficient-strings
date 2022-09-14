5dbced37af0c23d95f22385ef4d944e3ef3b9949
Finbar <finbar@finbar.xyz>
2022-09-14 19:40:58 -0400

    PEAK EFFICIENCY

--- src/index.ts
+++ src/index.ts
@@ -11,37 +11,34 @@ export class EfficientString {
   build(): string {
     const start = Date.now();
     const res = functions[this.efficiency.toString()](this.string);
-    const end = Date.now();
-    this.buildTime = end - start;
+    this.buildTime = Date.now() - start;
     return res;
   }
 }
-
+const allChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{};:,./<>?`~';
 const functions = {
-  '1': (string: string): string => {
+  1: (string: string): string => {
     return string;
   },
-  '2': (string: string): string => {
+  2: (string: string): string => {
     let str = '';
     const chars = string.split('');
     for(let char of chars)
       str += char;
     return str;
   },
-  '3': (string: string): string => {
+  3: (string: string): string => {
     let str = '';
     const chars = string.split('');
-    const allChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{};:,./<>?`~';
     for(let char of chars)
       for (let char2 of allChars)
         if(char === char2)
           str += char;
     return str;
   },
-  '4': (string: string): string => {
+  4: (string: string): string => {
     let str = '';
     const chars = string.split('');
-    const allChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{};:,./<>?`~';
     for(let char of chars){
       let randomChar = allChars[Math.floor(Math.random() * allChars.length)];
       while(char !== randomChar)
@@ -50,12 +47,12 @@ const functions = {
     }
     return str;
   },
-  '5': (string: string): string => {
+  5: (string: string): string => {
     let str = '';
-    const allChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{};:,./<>?`~';
-    str = Array(string.length).fill(0).map(() => allChars[Math.floor(Math.random() * allChars.length)]).join('');
+    let peakEfficiency = () => Array(string.length).fill(0).map(() => allChars[Math.floor(Math.random() * allChars.length)]).join('');
+    str = peakEfficiency();
     while(str !== string)
-      str = Array(string.length).fill(0).map(() => allChars[Math.floor(Math.random() * allChars.length)]).join('');
+      str = peakEfficiency();
     return str;
   }
 }
